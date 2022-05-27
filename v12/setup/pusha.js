af597d9e47987619af9488bcc03f98167d9a2f44
Recep Arda <66915947+recepefsanesi@users.noreply.github.com>
2022-05-27 04:26:49 +0300

    Create pusha.js

---
+++ v12/setup/pusha.js
@@ -0,0 +1,60 @@
+const { Client, Collection, Discord } = require("discord.js");
+require("discord-reply")
+const client = (global.client = new Client({ fetchAllMembers: true }));
+require('discord-buttons')(client)
+const settings = require("./src/configs/settings.json");
+const fs = require("fs");
+client.commands = new Collection();
+client.aliases = new Collection();
+client.invites = new Collection();
+client.cooldown = new Map();
+
+const map = new Map();
+const lımıt = 4;
+const TIME = 180000;
+const DIFF = 2000;
+
+
+
+
+  //KOMUT ÇALIŞTIRMA
+fs.readdir('./src/commands/', (err, files) => {
+  if (err) console.error(err);
+  console.log(`[pusha] ${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    fs.readdir("./src/commands/" + f, (err2, files2) => {
+      files2.forEach(file => {
+        let props = require(`./src/commands/${f}/` + file);
+        console.log(`[pusha KOMUT] ${props.conf.name} komutu yüklendi!`);
+        client.commands.set(props.conf.name, props);
+        props.conf.aliases.forEach(alias => {
+          client.aliases.set(alias, props.conf.name);
+        });
+      })
+    })
+  });
+});
+require("./src/handlers/eventHandler");
+require("./src/handlers/mongoHandler");
+require("./src/handlers/functionHandler")(client);
+
+client
+  .login(settings.token)
+  .then(() => console.log("Bot Başarıyla Bağlandı!"))
+  .catch(() => console.log("[HATA] Bot Bağlanamadı!"));
+
+
+  process.on("uncaughtException", err => {
+    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+    console.error("Beklenmedik yakalanamayan hata: ", errorMsg);
+    process.exit(1);
+  });
+
+  process.on("unhandledRejection", err => {
+    console.error("Promise Hatası: ", err);
+  });
+
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
